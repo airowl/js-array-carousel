4bf761337d212b67c46412dcf65cc655766bb6e0
rowl <airowl.gasga@gmail.com>
2022-03-01 16:03:41 +0100

    creato i primi commenti istruzione da fare

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello world!!');
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+/* 
+* a) creare un event listener per i due bottoni (previous e next)
+*
+* b) dobbiamo inserire alla classe main-img l'immagine
+*       - utilizziamo append()
+* 
+*
+*
+*/
+
+const btnPrevious = document.querySelector('.previous');
+const btnNext = document.querySelector('.next');
